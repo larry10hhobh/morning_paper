60142b5468b2bf985a6bfeacd277eecda7f1aae6
larry10hhobh <gong1004@126.com>
2023-04-03 00:41:19 +0800

    weather message

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"strconv"
+	"text/tabwriter"
+	"time"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -15,28 +19,57 @@ func init() {
 }
 
 func main() {
-	_, err := tgbotapi.NewBotAPI(config.TelegramBotToken)
+	bot, err := tgbotapi.NewBotAPI(config.TelegramBotToken)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("Bot username: %s\n", bot.Self.UserName)
+	fmt.Printf("Bot ID: %d\n", bot.Self.ID)
 
 	// Fetch weather and news
-	// weather := getWeather()
+	daily, err := getWeather(-1, 8)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var buf bytes.Buffer
+	w := tabwriter.NewWriter(&buf, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+	fmt.Fprintln(w, "日期\t星期\t最高温度\t最低温度\t白天\t晚上\t降水概率\t降水量")
+
+	for _, day := range daily {
+		precip, err := strconv.ParseFloat(day.Precip, 32)
+		if err != nil {
+			return
+		}
+		rainfall, err := strconv.ParseFloat(day.Rainfall, 32)
+		if err != nil {
+			return
+		}
+		date, err := time.Parse("2006-01-02", day.Date)
+		if err != nil {
+			fmt.Println("Error parsing date:", err)
+			return
+		}
+		weekday := date.Weekday()
+		fmt.Fprintf(w, "%s\t%s\t%s度\t%s度\t%s\t%s\t%.2f\t%.2fmm\n", day.Date, weekday, day.High, day.Low, day.TextDay, day.TextNight, precip, rainfall)
+	}
+	w.Flush()
+	tableString := buf.String()
+	fmt.Println(tableString)
+
 	// news := getNews()
 
 	// Send weather and news to Telegram
-	// sendToTelegram(weather, news, bot)
+	sendToTelegram(tableString, bot)
 }
 
-
-func sendToTelegram(weather, news string, bot *tgbotapi.BotAPI) {
-	message := fmt.Sprintf("Weather: %s\nNews: %s", weather, news)
-	msg := tgbotapi.NewMessage(config.TelegramUserID, message)
+func sendToTelegram(weather string, bot *tgbotapi.BotAPI) {
+	//message := fmt.Sprintf("Weather: %s\nNews: %s", weather)
+	msg := tgbotapi.NewMessage(config.TelegramUserID, weather)
 
 	_, err := bot.Send(msg)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		log.Printf("Message sent: %s", message)
+		log.Printf("Message sent: %s", weather)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"strconv"
 	"testing"
+	"text/tabwriter"
+	"time"
 )
 
 func TestGetWeather(t *testing.T) {
@@ -14,7 +18,80 @@ func TestGetWeather(t *testing.T) {
 	if err != nil {
 		t.Errorf("getWeather() returned an error: %v", err)
 	}
+	/*	for _, day := range daily {
+		// style 1
+		//fmt.Printf("Date: %s, High: %s, Low: %s, Test Day: %s, Text Night: %s, precip: %s, rainfall: %s\n", day.Date, day.High, day.Low, day.TextDay, day.TextNight, day.Precip, day.Rainfall)
+
+		// style 2
+		precip, err := strconv.ParseFloat(day.Precip, 64)
+		if err != nil {
+			t.Errorf("getWeather() returned an error: %v", err)
+			return
+		}
+		rainfall, err := strconv.ParseFloat(day.Rainfall, 64)
+		if err != nil {
+			t.Errorf("getWeather() returned an error: %v", err)
+			return
+		}
+		date, err := time.Parse("2006-01-02", day.Date)
+		if err != nil {
+			t.Errorf("Error parsing date: %s", err)
+			return
+		}
+		fmt.Println(fmt.Sprintf("%s (%s)：最高%s度，最低%s度，白天%s，晚上%s，降水概率%.2f（仅国外城市?），降水量%.2f", day.Date, date.Weekday(), day.High, day.Low, day.TextDay, day.TextNight, precip, rainfall))
+	}*/
+
+	/*	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+		fmt.Fprintln(w, "日期\t星期\t最高温\t最低温\t白天\t晚上\t降水概率\t降水量")
+
+		for _, day := range daily {
+			precip, err := strconv.ParseFloat(day.Precip, 64)
+			if err != nil {
+				t.Errorf("getWeather() returned an error: %v", err)
+				return
+			}
+			rainfall, err := strconv.ParseFloat(day.Rainfall, 64)
+			if err != nil {
+				t.Errorf("getWeather() returned an error: %v", err)
+				return
+			}
+			date, err := time.Parse("2006-01-02", day.Date)
+			if err != nil {
+				fmt.Println("Error parsing date:", err)
+				return
+			}
+			weekday := date.Weekday()
+			fmt.Fprintf(w, "%s\t%s\t%s度\t%s度\t%s\t%s\t%.2f\t%.2f\n", day.Date, weekday, day.High, day.Low, day.TextDay, day.TextNight, precip, rainfall)
+		}
+
+		w.Flush()*/
+
+	var buf bytes.Buffer
+	w := tabwriter.NewWriter(&buf, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+	fmt.Fprintln(w, "日期\t星期\t最高温度\t最低温度\t白天\t晚上\t降水概率\t降水量")
+
 	for _, day := range daily {
-		fmt.Printf("Date: %s, High: %s, Low: %s, Test Day: %s, Text Night: %s, precip: %s, rainfall: %s\n", day.Date, day.High, day.Low, day.TextDay, day.TextNight, day.Precip, day.Rainfall)
+		precip, err := strconv.ParseFloat(day.Precip, 32)
+		if err != nil {
+			t.Errorf("getWeather() returned an error: %v", err)
+			return
+		}
+		rainfall, err := strconv.ParseFloat(day.Rainfall, 32)
+		if err != nil {
+			t.Errorf("getWeather() returned an error: %v", err)
+			return
+		}
+		date, err := time.Parse("2006-01-02", day.Date)
+		if err != nil {
+			fmt.Println("Error parsing date:", err)
+			return
+		}
+		weekday := date.Weekday()
+		fmt.Fprintf(w, "%s\t%s\t%s度\t%s度\t%s\t%s\t%.2f\t%.2fmm\n", day.Date, weekday, day.High, day.Low, day.TextDay, day.TextNight, precip, rainfall)
 	}
+
+	w.Flush()
+
+	tableString := buf.String()
+	fmt.Println(tableString)
 }
